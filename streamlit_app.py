9839b46652d993c1ae1da83be3083feb9f2d77b5
barbssilva <barbarasilva@anglotex.pt>
2025-12-05 09:25:07 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Por favor, selecione o cliente!!")
+
+st.set_page_config(
+    page_title ="Multipage App",
+    page_icon="😁",
 )
+
+st.title("Main Page")
+st.sidebar.success(2Select a page above.")
