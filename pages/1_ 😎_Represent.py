d17a61363b23242fa0a5ef91d618c078b99586df
barbssilva <barbarasilva@anglotex.pt>
2025-12-05 10:22:21 +0000

    Update 1_ 😎_Represent.py

--- pages/1_ 😎_Represent.py
+++ pages/1_ 😎_Represent.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Represent")
 
 ficha_custo = st.file_uploader(
-    "Por favor, carregue o ficheiro pdf",
+    "Por favor, carregue o ficheiro pdf.",
     type=["pdf"],
     key="uploader_pdf"
 )
