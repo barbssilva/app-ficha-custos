1d03c98cb038813694e1879308d418518d985d25
barbssilva <barbarasilva@anglotex.pt>
2025-12-05 10:16:07 +0000

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.title("<<<<-----")
-
 st.set_page_config(
     page_title ="Fichas custos",
     page_icon="😁",
 )
 
-st.title("Por favor, selecione o cliente!!")
+st.title("<-- Por favor, selecione o cliente!!")
 st.sidebar.success("Selecione uma página acima")
