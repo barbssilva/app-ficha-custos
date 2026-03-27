b8d0932b79462869f840ff188542cc3a5fb1b7da
barbssilva <barbarasilva@anglotex.pt>
2026-03-27 12:15:48 +0000

    Update import path for Excel processing functions

--- pages/2_😜_SlamJam.py
+++ pages/2_😜_SlamJam.py
@@ -11,7 +11,7 @@ import zipfile
 
 st.title("Slam Jam")
 
-from fichas_custos_excel_represent import trim_excel_before_marker, pdf_to_excel, add_images
+from fichas_custos_excel_slj import trim_excel_before_marker, pdf_to_excel, add_images
 
 st.write("Pode carregar vários ficheiros pdf de uma só vez, no fim fará download de um ficheiro .zip com todos os ficheiros excel")
 
