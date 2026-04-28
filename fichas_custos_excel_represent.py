0e13b94bbc681724d9990719afc83db44b6b93e7
barbssilva <barbarasilva@anglotex.pt>
2026-04-28 17:01:17 +0100

    Update fichas_custos_excel_represent.py

--- fichas_custos_excel_represent.py
+++ fichas_custos_excel_represent.py
@@ -273,7 +273,7 @@ def trim_excel_before_marker(excel_path,excel_saida):
         acessorios_cost = pd.to_numeric(df2.iloc[acessorios_idx, 2], errors='coerce')
         perc_acessorios = acessorios_cost * percent_value
 
-        codes_key = ["1957","APL0130","ECT0185","EMC0104","FCM0138","SAC0027"]
+        codes_key = ["1957","APL0130","ECT0185","EMC0104","FCM0138","SAC0027","ECP0076","ECT0205","EMC0098","SAC0073","SAC0104"]
         #sheet que contem os acessorios descriminados
         sheet5_name = list(sheets.keys())[4]
         #garante que todos os valores são string e sem NaN (substitui NaN pela string "")
