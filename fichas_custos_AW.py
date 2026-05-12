d5dedf2c42bbb6ad1b3f99a79b4d07dc3a77b08c
barbssilva <barbarasilva@anglotex.pt>
2026-05-12 11:26:00 +0100

    Update fichas_custos_AW.py

--- fichas_custos_AW.py
+++ fichas_custos_AW.py
@@ -105,7 +105,7 @@ def trim_excel_before_marker(excel_path,excel_saida):
         for i in range(0,len(df5)):
             if df5.iloc[i,0] in codes_key:
                 nominataded_acessorios_cost += pd.to_numeric(df5.iloc[i, -1], errors='coerce')
-                linhas_nominated.append(df5.iloc[i]
+                linhas_nominated.append(df5.iloc[i])
             else:
                 linhas_other_trims.append(df5.iloc[i]
         perc_acessorios = nominataded_acessorios_cost * percent_value
